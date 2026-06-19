1c7bede3d75184cd26fd605b8691661247844c71
nishantksingh0 <nishant18x.org@gmail.com>
2026-06-19 21:23:38 +0530

    Updated images

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ Character-Level Decoder Inference Pipeline
 
 This repo demonstrates the inference pipeline for the trained
 Character-Level Decoder-Only language model.
-
-Originally developed in Google Colab:
-https://colab.research.google.com/drive/1CjvzQ7QYwtLuCXRpqcB29o5tAKjGgJrg
 """
 
 from inference import generate
